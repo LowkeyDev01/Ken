ffe1aef37fd583db870a6ccb0b784b47d0aefe26
Ken <kananaya335@gmail.com>
2026-09-18 21:08:29 +0100

    image cropping shii

--- src/pages/ContactSection.jsx
+++ src/pages/ContactSection.jsx
@@ -206,7 +206,7 @@ export default function Contact() {
                             duration: 1,
                             ease: "easeOut",
                         }}
-                        className="absolute bottom-0 h-[55vh] md:h-[90vh] w-auto max-w-none"
+                        className="absolute bottom-0 h-[44vh] md:h-[90vh] w-auto max-w-none"
                     />
                 </motion.div>
             </div>
