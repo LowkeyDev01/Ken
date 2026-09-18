3cb6aeef02403a115d77b1483c53b95d6b9ba4b2
Ken <kananaya335@gmail.com>
2026-09-18 07:35:59 +0100

    Tweaked the Home section

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,11 +6,11 @@ export default function Navbar(){
             <div className="flex justify-start py-3 items-center">
                 <p className="font-bold md:text-3xl font-six text-3xl pl-3 md:pl-10">K.</p>
             </div>
-            <div className="flex justify-end font-seven gap-12 md:pr-20 font- items-center">
-                <a href="#" className="hidden md:block text-xs opacity-100 border border-transparent border-b-black py-1.5">Home</a>
-                <a href="#" className="hidden md:block text-xs opacity-70 border border-transparent hover:border-b-black duration-300 py-1.5">About</a>
-                <a href="#" className="hidden md:block text-xs opacity-70 border border-transparent hover:border-b-black duration-300 py-1.5">Projects</a>
-                <a href="#" className="hidden md:block text-xs opacity-70 border border-transparent hover:border-b-black duration-300 py-1.5">Contact</a>
+            <div className="flex justify-end font-seven gap-8 md:pr-20 font- items-center">
+                <a href="#" className="hidden md:block text-[0.7em] opacity-100 border border-transparent border-b-black py-1.5">Home</a>
+                <a href="#" className="hidden md:block text-[0.7em] opacity-70 border border-transparent hover:border-b-black duration-300 py-1.5">About</a>
+                <a href="#" className="hidden md:block text-[0.7em] opacity-70 border border-transparent hover:border-b-black duration-300 py-1.5">Projects</a>
+                <a href="#" className="hidden md:block text-[0.7em] opacity-70 border border-transparent hover:border-b-black duration-300 py-1.5">Contact</a>
                 <GiHamburgerMenu className="md:hidden text-3xl mr-2" />
             </div>
         </div>
